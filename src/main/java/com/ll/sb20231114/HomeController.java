be40e6671448c90701b39b82976a5b5f96f5f90e
schim613 <uximin@naver.com>
2023-11-14 15:18:54 +0900

    calc3 ~ 5

--- src/main/java/com/ll/sb20231114/HomeController.java
+++ src/main/java/com/ll/sb20231114/HomeController.java
@@ -2,6 +2,7 @@ package com.ll.sb20231114;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // 컨트롤러
@@ -19,5 +20,46 @@ public class HomeController {
     String showAbout() {
         System.out.println("배고파!!!");
         return "개발자 커뮤니티";
+    }
+
+    @GetMapping("/calc")
+    @ResponseBody
+    String showCalc(int a, int b) { // null을 허용하지 않는 기본 타입이다.
+        System.out.println("냠냠");
+        return "계산 결과 : %d".formatted(a + b);
+    }
+
+    @GetMapping("/calc2")
+    @ResponseBody
+    String showCalc2(Integer a, Integer b) { // null을 허용하는 참조 타입이다.
+        System.out.println("쩝쩝");
+        return "a : " + a + ", b : " + b;
+    }
+
+    @GetMapping("/calc3") // url은 무조건 문자열이기 때문에 숫자도 문자열로
+    @ResponseBody
+    String showCalc3(
+            @RequestParam(defaultValue = "0") int a,
+            @RequestParam(defaultValue = "0") int b
+    ) {
+        return "계산 결과 : %d".formatted(a + b);
+    }
+
+    @GetMapping("/calc4")
+    @ResponseBody
+    String showCalc4(
+            @RequestParam(defaultValue = "0") double a,
+            @RequestParam(defaultValue = "0") double b
+    ) {
+        return "계산 결과 : %f".formatted(a + b);
+    }
+
+    @GetMapping("/calc5")
+    @ResponseBody
+    String showCalc5(
+            @RequestParam(defaultValue = "-") String a,
+            @RequestParam(defaultValue = "-") String b
+    ) {
+        return "계산 결과 : %s".formatted(a + b);
     }
 }
