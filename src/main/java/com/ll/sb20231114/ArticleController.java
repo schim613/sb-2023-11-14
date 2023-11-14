1551075a6ee63af9a3f2ae7e34e6aa11dbe72ac2
schim613 <uximin@naver.com>
2023-11-14 22:12:27 +0900

    article/write, step 2

--- src/main/java/com/ll/sb20231114/ArticleController.java
+++ src/main/java/com/ll/sb20231114/ArticleController.java
@@ -2,6 +2,7 @@ package com.ll.sb20231114;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 // 액션 컨트롤러들을 한 곳에 모아 써도 되고, 나눠 써도 된다.
 // 하지만 쓰임에 맞게 나누면 좋음!
@@ -10,5 +11,14 @@ public class ArticleController {
     @GetMapping("/article/write")
     String showWrite() {
         return "article/write";
+    }
+
+    @GetMapping("/article/doWrite")
+    @ResponseBody
+    String doWrite(
+            String title,
+            String body
+    ) {
+        return "게시물이 작성되었습니다.";
     }
 }
