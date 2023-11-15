e1ee255c1d07d49656e42a71d45384d16806e578
schim613 <uximin@naver.com>
2023-11-15 09:28:58 +0900

    article/write, step 4

--- src/main/java/com/ll/sb20231114/ArticleController.java
+++ src/main/java/com/ll/sb20231114/ArticleController.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
 // 액션 컨트롤러들을 한 곳에 모아 써도 되고, 나눠 써도 된다.
 // 하지만 쓰임에 맞게 나누면 좋음!
 @Controller
@@ -17,13 +20,17 @@ public class ArticleController {
 
     @GetMapping("/article/doWrite")
     @ResponseBody
-    Article doWrite(
+    Map<String, Object> doWrite(
             String title,
             String body
     ) {
         Article article = new Article(1, title, body);
 
-        return article;
+        Map<String, Object> rs = new HashMap<>();
+        rs.put("msg", "1번 게시물이 작성되었습니다.");
+        rs.put("data", article);
+
+        return rs;
     }
 }
 
