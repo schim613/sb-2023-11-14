6b55594b969feefe7f9e9c4bb9591802a98ea4a2
schim613 <uximin@naver.com>
2023-11-15 14:13:26 +0900

    step 14, submitWriteForm

--- src/main/java/com/ll/sb20231114/ArticleController.java
+++ src/main/java/com/ll/sb20231114/ArticleController.java
@@ -6,60 +6,46 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+
 
 // 액션 컨트롤러들을 한 곳에 모아 써도 되고, 나눠 써도 된다.
 // 하지만 쓰임에 맞게 나누면 좋음!
 @Controller
 public class ArticleController {
-    private List<Article> articles = new ArrayList<>(); // 전역 변수 = 인스턴스 변수
+    private List<Article> articles = new ArrayList<>();
 
+    //    GET /article/write
     @GetMapping("/article/write")
     String showWrite() {
         return "article/write";
     }
 
+//    GET /article/doWrite?title=제목&body=내용
     @GetMapping("/article/doWrite")
     @ResponseBody
-    RsData doWrite(
-            String title,
-            String body
-    ) {
-        Article article = new Article(articles.size() + 1, title, body);
+    RsData<Article> doWrite(String title, String body) {
+        long id = articles.size() + 1;
+
+        Article article = new Article(id, title, body);
         articles.add(article);
 
-        RsData<Article> rs = new RsData<>(
-                "S-1",
-                "%d번 게시물이 작성되었습니다.".formatted(article.getId()),
-                article
-        );
-
-        String resultCode = rs.getResultCode();
-        String mgs = rs.getMsg();
-        Article _article = rs.getData();
-
-        return rs;
+        return new RsData<>("S-1", "성공", article);
     }
 
+//    GET /article/getLastArticle
     @GetMapping("/article/getLastArticle")
     @ResponseBody
-    Article getLastArticle () {
+    Article getLastArticle() {
         return articles.getLast();
     }
-
+//    GET /article/getArticles
     @GetMapping("/article/getArticles")
     @ResponseBody
-    List<Article> getArticles () {
+    List<Article> getArticles() {
         return articles;
     }
-}
-
-@AllArgsConstructor
-@Getter
-class RsData<T> {
-    private String resultCode;
-    private String msg;
-    private T data;
 }
 
 @AllArgsConstructor
@@ -68,4 +54,12 @@ class Article {
     private long id;
     private String title;
     private String body;
+}
+
+@AllArgsConstructor
+@Getter
+class RsData<T> {
+    private String resultCode;
+    private String msg;
+    private T data;
 }
