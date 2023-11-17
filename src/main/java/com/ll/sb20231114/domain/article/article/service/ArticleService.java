10d8a81b8662f051e2a813ceb7f6e8b0bc8b8f7e
schim613 <uximin@naver.com>
2023-11-17 14:20:15 +0900

    step 36, constructor injection

--- src/main/java/com/ll/sb20231114/domain/article/article/service/ArticleService.java
+++ src/main/java/com/ll/sb20231114/domain/article/article/service/ArticleService.java
@@ -3,14 +3,21 @@ package com.ll.sb20231114.domain.article.article.service;
 import com.ll.sb20231114.domain.article.article.entity.Article;
 import com.ll.sb20231114.domain.article.article.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Component // 단 한번만 생성되고, 그 이후에는 재사용되는 객체임
+@Service // 단 한번만 생성되고, 그 이후에는 재사용되는 객체임
 public class ArticleService {
+
+    // @Autowired 필드 주입
+    private final ArticleRepository articleRepository;
+
+    // 생성자 주입
     @Autowired
-    private ArticleRepository articleRepository;
+    public ArticleService(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
 
     public Article write(String title, String body) {
         Article article = new Article(title, body);
