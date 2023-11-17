edaf324eead486b177f38030fad29840ac23d1c7
schim613 <uximin@naver.com>
2023-11-17 14:31:54 +0900

    step 38, @RequiredArgsConstructor

--- src/main/java/com/ll/sb20231114/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/sb20231114/domain/article/article/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.ll.sb20231114.domain.article.article.controller;
 import com.ll.sb20231114.domain.article.article.entity.Article;
 import com.ll.sb20231114.domain.article.article.service.ArticleService;
 import com.ll.sb20231114.global.rsData.RsData;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,14 +16,16 @@ import java.util.List;
 // 액션 컨트롤러들을 한 곳에 모아 써도 되고, 나눠 써도 된다.
 // 하지만 쓰임에 맞게 나누면 좋음!
 @Controller
+@RequiredArgsConstructor // final 필드들(입력받은 생성자)을 자동으로 생성해라
 public class ArticleController {
     @Autowired // 필드 주입, final은 뺀다.
     private final ArticleService articleService;
 
-    // @Autowired // 생성자 주입
-    public ArticleController(ArticleService articleService) {
-        this.articleService = articleService;
-    }
+    // @Autowired는 생성자가 하나면 생략 가능
+    // 생성자 주입 => @RequiredArgsConstructor로 자동 생성되어 생략 가능
+    // public ArticleController(ArticleService articleService) {
+    //     this.articleService = articleService;
+    // }
 
     //    GET /article/write
     @GetMapping("/article/write")
--- src/main/java/com/ll/sb20231114/domain/article/article/service/ArticleService.java
+++ src/main/java/com/ll/sb20231114/domain/article/article/service/ArticleService.java
@@ -2,19 +2,22 @@ package com.ll.sb20231114.domain.article.article.service;
 
 import com.ll.sb20231114.domain.article.article.entity.Article;
 import com.ll.sb20231114.domain.article.article.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service // 단 한번만 생성되고, 그 이후에는 재사용되는 객체임
+@RequiredArgsConstructor
 public class ArticleService {
     // @Autowired 필드 주입
     private final ArticleRepository articleRepository;
 
-    // @Autowired 생략 가능
-    public ArticleService(ArticleRepository articleRepository) { // 생성자 주입
-        this.articleRepository = articleRepository;
-    }
+    // 생성자 주입
+    // @Autowired는 생성자가 하나면 생략 가능
+    // public ArticleService(ArticleRepository articleRepository) {
+    //     this.articleRepository = articleRepository;
+    // }
 
     public Article write(String title, String body) {
         Article article = new Article(title, body);
