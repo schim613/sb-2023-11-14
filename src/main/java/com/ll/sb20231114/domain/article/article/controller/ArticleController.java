9af562f5294f8bf4e6ac086ef5c459d9118a219d
schim613 <uximin@naver.com>
2023-11-20 16:43:32 +0900

    정리

--- src/main/java/com/ll/sb20231114/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/sb20231114/domain/article/article/controller/ArticleController.java
@@ -2,9 +2,6 @@ package com.ll.sb20231114.domain.article.article.controller;
 
 import com.ll.sb20231114.domain.article.article.entity.Article;
 import com.ll.sb20231114.domain.article.article.service.ArticleService;
-import com.ll.sb20231114.global.rq.Rq;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -15,7 +12,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -27,13 +23,49 @@ import java.util.List;
 public class ArticleController {
     // @Autowired 필드 주입, final은 뺀다.
     private final ArticleService articleService;
-    private final Rq rq;
 
-    // @Autowired 는 생성자가 하나면 생략 가능
-    // 생성자 주입 => @RequiredArgsConstructor로 자동 생성되어 생략 가능
-    // public ArticleController(ArticleService articleService) {
-    //     this.articleService = articleService;
-    // }
+    //    GET /article/doWrite?title=제목&body=내용
+    @GetMapping("/article/list")
+    String showList(Model model) {
+        List<Article> articles = articleService.findAll();
+
+        model.addAttribute("articles", articles);
+
+        return "article/list";
+    }
+
+    @GetMapping("/article/delete/{id}")
+    String delete(@PathVariable long id) {
+        articleService.delete(id);
+
+        String msg = "id %d, article is deleted".formatted(id);
+
+        return "redirect:/article/list?msg=" + msg;
+    }
+
+    //    GET /article/write
+    @GetMapping("/article/write")
+    String showWrite() {
+        return "article/write";
+    }
+
+    @Data
+    public static class WriteForm {
+        @NotBlank(message = "제목 좀..")
+        private String title;
+        @NotBlank @NotNull
+        private String body;
+    }
+
+    //    GET /article/doWrite?title=제목&body=내용
+    @PostMapping("/article/write")
+    String write(@Valid WriteForm writeForm) {
+        Article article = articleService.write(writeForm.title, writeForm.body);
+
+        String msg = "id %d, article is created".formatted(article.getId());
+
+        return "redirect:/article/list?msg=" + msg;
+    }
 
     @GetMapping("/article/modify/{id}")
     String showModify(Model model, @PathVariable long id) {
@@ -61,15 +93,6 @@ public class ArticleController {
         return "redirect:/article/list?msg=" + msg;
     }
 
-    @GetMapping("/article/delete/{id}")
-    String delete(@PathVariable long id) {
-        articleService.delete(id);
-
-        String msg = "id %d, article is deleted".formatted(id);
-
-        return "redirect:/article/list?msg=" + msg;
-    }
-
     @GetMapping("/article/detail/{id}")
     String showDetail(Model model, @PathVariable long id) {
         Article article = articleService.findById(id).get();
@@ -77,87 +100,5 @@ public class ArticleController {
         model.addAttribute("article", article);
 
         return "article/detail";
-    }
-
-    //    GET /article/write
-    @GetMapping("/article/write")
-    String showWrite() {
-        return "article/write";
-    }
-
-    @Data
-    public static class WriteForm {
-        @NotBlank(message = "제목 좀..")
-        private String title;
-        @NotBlank @NotNull
-        private String body;
-    }
-
-    //    GET /article/doWrite?title=제목&body=내용
-    @PostMapping("/article/write")
-    String write(@Valid WriteForm writeForm) {
-        Article article = articleService.write(writeForm.title, writeForm.body);
-
-        String msg = "id %d, article is created".formatted(article.getId());
-
-        return "redirect:/article/list?msg=" + msg;
-    }
-
-    //    GET /article/doWrite?title=제목&body=내용
-    @GetMapping("/article/list")
-    String showList(Model model) {
-        List<Article> articles = articleService.findAll();
-
-        model.addAttribute("articles", articles);
-
-        return "article/list";
-    }
-
-    //    GET /article/getLastArticle
-    @GetMapping("/article/getLastArticle")
-    @ResponseBody
-    Article getLastArticle() {
-        return articleService.findLastArticle();
-    }
-
-    //    GET /article/getArticles
-    @GetMapping("/article/getArticles")
-    @ResponseBody
-    List<Article> getArticles() {
-        return articleService.findAll();
-    }
-
-    @GetMapping("/article/articleServicePointer")
-    @ResponseBody
-    String articleServicePointer() {
-        return articleService.toString(); // 객체 주소가 같음 => 스프링부트가 만들어질 때, 객체를 딱 한개 만들고 계속 사용
-    }
-
-    // 리퀘스트와 리스폰스는 요청이 있을 때 잠깐 생겼다가 사라짐
-    @GetMapping("/article/httpServletRequestPointer")
-    @ResponseBody
-    String httpServletRequestPointer(HttpServletRequest req) {
-        return req.toString();
-    }
-
-    @GetMapping("/article/httpServletResponsePointer")
-    @ResponseBody
-    String httpServletResponsePointer(HttpServletResponse resp) {
-        return resp.toString();
-    }
-
-    @GetMapping("/article/rqPointer")
-    @ResponseBody
-    String rqPointer() {
-        return rq.toString();
-    }
-
-    @GetMapping("/article/rqTest")
-    String showRqTest(Model model) {
-        String rqToString = rq.toString();
-
-        model.addAttribute("rqToString", rqToString);
-
-        return "article/rqTest";
     }
 }
