54de3137fb6f628ffb3eb12cca16c473432d8d4c
schim613 <uximin@naver.com>
2023-11-20 14:27:26 +0900

    step 66, test data

--- src/main/java/com/ll/sb20231114/domain/article/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/sb20231114/domain/article/article/repository/ArticleRepository.java
@@ -4,12 +4,17 @@ import com.ll.sb20231114.domain.article.article.entity.Article;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class ArticleRepository {
-    private final List<Article> articles;
+    private final List<Article> articles = new ArrayList<>(){{
+        add(new Article(1L, "제목1", "내용1"));
+        add(new Article(2L, "제목2", "내용2"));
+        add(new Article(3L, "제목3", "내용3"));
+    }};
 
     public Article save(Article article) {
         if (article.getId() == 0) {
