2b3f549d1f9b9a79cfc95d1739d9a77edfc1af36
schim613 <uximin@naver.com>
2023-11-22 00:41:07 +0900

    step 5, addCookie

--- src/main/java/com/ll/sb20231114/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/sb20231114/domain/member/member/controller/MemberController.java
@@ -3,6 +3,8 @@ package com.ll.sb20231114.domain.member.member.controller;
 import com.ll.sb20231114.domain.member.member.entity.Member;
 import com.ll.sb20231114.domain.member.member.service.MemberService;
 import com.ll.sb20231114.global.rq.Rq;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -32,7 +34,7 @@ public class MemberController {
     }
 
     @PostMapping("/member/login")
-    String join(@Valid LoginForm loginForm) {
+    String join(@Valid LoginForm loginForm, HttpServletResponse response) {
         Member member = memberService.findByUsername(loginForm.username).get();
 
         if (!member.getPassword().equals(loginForm.password)) {
@@ -40,6 +42,9 @@ public class MemberController {
         }
 
         // 로그인 처리 => 쿠키로 할 것임
+        Cookie cookie = new Cookie("loginedMemberId", member.getId() + "");
+        cookie.setPath("/");
+        response.addCookie(cookie);
 
         return rq.redirect("/article/list", "로그인이 완료되었습니다.");
     }
