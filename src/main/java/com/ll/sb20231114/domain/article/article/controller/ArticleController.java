1d2c593e03e30854a074e8db6c3641ca8d579f84
schim613 <uximin@naver.com>
2023-11-22 00:55:13 +0900

    step 6, user1 welcome

--- src/main/java/com/ll/sb20231114/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/sb20231114/domain/article/article/controller/ArticleController.java
@@ -2,6 +2,8 @@ package com.ll.sb20231114.domain.article.article.controller;
 
 import com.ll.sb20231114.domain.article.article.entity.Article;
 import com.ll.sb20231114.domain.article.article.service.ArticleService;
+import com.ll.sb20231114.domain.member.member.entity.Member;
+import com.ll.sb20231114.domain.member.member.service.MemberService;
 import com.ll.sb20231114.global.rq.Rq;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
@@ -24,14 +26,17 @@ import java.util.List;
 public class ArticleController {
     // @Autowired 필드 주입, final은 뺀다.
     private final ArticleService articleService;
+    private final MemberService memberService;
     private final Rq rq;
 
     //    GET /article/doWrite?title=제목&body=내용
     @GetMapping("/article/list")
     String showList(Model model) {
+        Member loginedMember = memberService.findById(1L).get();
         List<Article> articles = articleService.findAll();
 
         model.addAttribute("articles", articles);
+        model.addAttribute("loginedMember", loginedMember);
 
         return "article/article/list";
     }
@@ -39,6 +44,7 @@ public class ArticleController {
     @GetMapping("/article/detail/{id}")
     String showDetail(Model model, @PathVariable long id) {
         Article article = articleService.findById(id).get();
+
 
         model.addAttribute("article", article);
 
