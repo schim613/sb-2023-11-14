37381dbb9b7ad7884174f04eb7e5416bef866f5e
schim613 <uximin@naver.com>
2023-11-22 10:22:29 +0900

    step 7, loginedMemberId Cookie

--- src/main/java/com/ll/sb20231114/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/sb20231114/domain/article/article/controller/ArticleController.java
@@ -5,9 +5,10 @@ import com.ll.sb20231114.domain.article.article.service.ArticleService;
 import com.ll.sb20231114.domain.member.member.entity.Member;
 import com.ll.sb20231114.domain.member.member.service.MemberService;
 import com.ll.sb20231114.global.rq.Rq;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -16,7 +17,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 
 // 액션 컨트롤러들을 한 곳에 모아 써도 되고, 나눠 써도 된다.
@@ -31,12 +34,25 @@ public class ArticleController {
 
     //    GET /article/doWrite?title=제목&body=내용
     @GetMapping("/article/list")
-    String showList(Model model) {
-        Member loginedMember = memberService.findById(1L).get();
+    String showList(Model model, HttpServletRequest req) {
+        // 쿠키 이름이 loginedMemberId 인 것의 값을 가져와서 long 타입으로 변환, 만약에 그런게 없다면 0을 반환
+        long loginedMemberId = Optional.ofNullable(req.getCookies())
+                .stream()
+                .flatMap(Arrays::stream)
+                .filter(cookie -> cookie.getName().equals("loginedMemberId"))
+                .map(Cookie::getValue)
+                .mapToLong(Long::parseLong)
+                .findFirst()
+                .orElse(0);
+
+        if (loginedMemberId > 0) {
+            Member loginedMember = memberService.findById(loginedMemberId).get();
+            model.addAttribute("loginedMember", loginedMember);
+        }
+
         List<Article> articles = articleService.findAll();
 
         model.addAttribute("articles", articles);
-        model.addAttribute("loginedMember", loginedMember);
 
         return "article/article/list";
     }
@@ -61,7 +77,7 @@ public class ArticleController {
     public static class WriteForm {
         @NotBlank(message = "제목 좀..")
         private String title;
-        @NotBlank @NotNull
+        @NotBlank
         private String body;
     }
 
