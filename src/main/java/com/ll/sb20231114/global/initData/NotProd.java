92b56dd4558d115ea725708d312cead8401ed380
schim613 <uximin@naver.com>
2023-11-27 15:55:28 +0900

    step 28, NotProd

--- src/main/java/com/ll/sb20231114/global/initData/NotProd.java
+++ src/main/java/com/ll/sb20231114/global/initData/NotProd.java
@@ -1,5 +1,8 @@
 package com.ll.sb20231114.global.initData;
 
+import com.ll.sb20231114.domain.article.article.service.ArticleService;
+import com.ll.sb20231114.domain.member.member.entity.Member;
+import com.ll.sb20231114.domain.member.member.service.MemberService;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,11 +11,17 @@ import org.springframework.context.annotation.Configuration;
 public class NotProd {
     @Bean // 스프링부트가 시작될 때 실행이 돼서 initNotProd 이름의 빈이 등록됨
     public ApplicationRunner initNotProd( // 우리가 명시적으로 호출하지 않아도 프로그램 시작할 때 호출됨
-//            MemberRepository memberRepository,
-//            ArticleRepository articleRepository
+            MemberService memberService,
+            ArticleService articleService
     ) {
         return args -> {
-            System.out.println("안녕");
+            Member memberAdmin = memberService.join("admin", "1234");
+            Member memberUser1 = memberService.join("user1", "1234");
+            Member memberUser2 = memberService.join("user2", "1234");
+
+            articleService.write(memberAdmin, "제목1", "내용1");
+            articleService.write(memberUser1, "제목2", "내용2");
+            articleService.write(memberUser1, "제목3", "내용3");
         };
     }
 }
