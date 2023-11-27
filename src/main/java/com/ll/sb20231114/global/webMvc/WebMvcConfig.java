0eb25376e56d6ac333a54f921fd795621ea27c8c
schim613 <uximin@naver.com>
2023-11-27 17:30:54 +0900

    step 31, WebMvcConfig

---
+++ src/main/java/com/ll/sb20231114/global/webMvc/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.ll.sb20231114.global.webMvc;
+
+import com.ll.sb20231114.global.interceptor.NeedToAdminInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Autowired
+    private final NeedToAdminInterceptor needToAdminInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(needToAdminInterceptor)
+                .addPathPatterns("/adm/**");
+    }
+}
