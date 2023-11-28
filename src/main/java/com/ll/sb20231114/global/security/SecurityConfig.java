c9ce8b985e306adbc46b480b681506b05e996926
schim613 <uximin@naver.com>
2023-11-28 23:52:06 +0900

    step 6, csrf disable

--- src/main/java/com/ll/sb20231114/global/security/SecurityConfig.java
+++ src/main/java/com/ll/sb20231114/global/security/SecurityConfig.java
@@ -9,6 +9,10 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(
+                    csrf -> csrf.disable()
+                );
 
         return http.build();
     }
