b3fea8f372e14dbb9ceb90f6a1538b2151131506
schim613 <uximin@naver.com>
2023-11-29 00:02:22 +0900

    step 8, th:action

--- src/main/java/com/ll/sb20231114/global/security/SecurityConfig.java
+++ src/main/java/com/ll/sb20231114/global/security/SecurityConfig.java
@@ -9,10 +9,11 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(
-                    csrf -> csrf.disable()
-                );
+        // 스프링 시큐리티에서 csrf가 포함되어 있는데 csrf 토큰이 있어야 POST 전송이 됨
+//        http
+//                .csrf(
+//                    csrf -> csrf.disable()
+//                );
 
         return http.build();
     }
