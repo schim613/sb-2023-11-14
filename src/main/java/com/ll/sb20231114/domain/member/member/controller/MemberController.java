ba49bbe8e412949f5af4e06dbe320a8c3389c002
schim613 <uximin@naver.com>
2023-11-30 15:45:56 +0900

    step 14, member join

--- src/main/java/com/ll/sb20231114/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/sb20231114/domain/member/member/controller/MemberController.java
@@ -4,7 +4,6 @@ import com.ll.sb20231114.domain.member.member.service.MemberService;
 import com.ll.sb20231114.global.rq.Rq;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -35,7 +34,6 @@ public class MemberController {
         @NotBlank(message = "아이디를 입력해주세요.")
         private String username;
         @NotBlank
-        @NotNull
         private String password;
     }
 
