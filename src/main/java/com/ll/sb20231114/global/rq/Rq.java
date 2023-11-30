d8edfd32006af9f14c8124b77718faa8e7085316
schim613 <uximin@naver.com>
2023-11-30 13:42:34 +0900

    step 9, admin page menu

--- src/main/java/com/ll/sb20231114/global/rq/Rq.java
+++ src/main/java/com/ll/sb20231114/global/rq/Rq.java
@@ -76,6 +76,10 @@ public class Rq {
     }
 
     public boolean isAdmin() {
+        if (!isLogined()) {
+            return false;
+        }
+
         return user.getAuthorities()
                 .stream()
                 .anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"));
